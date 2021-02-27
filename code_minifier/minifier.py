c519ced82fb59eaec63bcdd26e11ab33f9e7d03c
= <royaleagle.dev@gmail.com>
2021-02-27 22:54:52 +0100

    code_minifier 1.0.1 -Adjust file naming convention

--- code_minifier/minifier.py
+++ code_minifier/minifier.py
@@ -2,6 +2,17 @@ import re
 import os
 
 
+def minify_file_name(file_name):
+    pattern = re.compile(r'([a-zA-Z]+)([.]{1})([a-zA-Z]{2,10})')
+    items = re.findall(pattern, file_name)
+    new_file_name = items[0][0]+'_min'+items[0][1]+items[0][2]
+    return new_file_name
+
+
+#print (minify_file_name('olaoluwa.css'))
+    
+
+
 def minify(my_file):
     patterns = {'spaces': r'\s'}
     data_string = ''
@@ -12,7 +23,7 @@ def minify(my_file):
             new_data = re.sub(patterns['spaces'], '', data[i])
             data_string += new_data
     file_name = os.path.basename(my_file)
-    new_file_name = 'min_'+file_name
+    new_file_name = minify_file_name(file_name)
     new_path = my_file.replace(file_name, new_file_name)
     return [data_string, new_path]
 
